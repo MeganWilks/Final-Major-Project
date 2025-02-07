cb3858abdeed351013f3e886415c086f0ca71b05
Megan Wilks <meganwilks18@gmail.com>
2025-02-07 16:41:41 +0000

    Initial Commit
    Starting Player Health Mechanic

---
+++ Assets/Scripts/HeartsHealthSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartsHealthSystem
+{
+
+    private List<Heart> heartList;
+
+    public HeartsHealthSystem(int heartsAmount)
+    {
+        heartList = new List<Heart>();
+        for (int i = 0; i < heartsAmount; i++)
+        {
+           Heart heart = new Heart(3);
+           heartList.Add(heart);
+        }
+
+        heartList[heartList.Count - 1].SetFragments(2);
+
+
+    }
+
+    public List<Heart> GetHeartList()
+    {
+        return heartList;
+    }
+
+    //Resembles 1 Heart
+    public class Heart
+    {
+        private int fragments;
+
+        public Heart (int fragments)
+        {
+            this.fragments = fragments;
+        }
+
+        public int GetFragementsAmount()
+        {
+            return fragments;
+        }
+
+        public void SetFragments (int fragments)
+        {
+            this.fragments = fragments;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/HeartsHealthVisual.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartsHealthVisual : MonoBehaviour
+{
+    #region Variables
+    [Header("Heart Sprites")]
+
+    [SerializeField] private Sprite fullHeartSprite;
+    [SerializeField] private Sprite halfHeartSprite;
+    [SerializeField] private Sprite emptyHeartSprite;
+
+    [Header("Lists")]
+
+    [SerializeField] private List<HeartImage> heartImageList;
+
+    [Header("Scripts")]
+
+    [SerializeField] private HeartsHealthSystem heartsHealthSystem;
+    #endregion
+
+    private void Awake()
+    {
+        #region Awake
+        heartImageList = new List<HeartImage>();
+        #endregion
+    }
+
+
+
+    private void Start()
+    {
+        #region Start
+        HeartsHealthSystem heartsHealthSystem = new HeartsHealthSystem(3);
+        SetHeartsHealthSystem(heartsHealthSystem);
+        #endregion
+
+    }
+
+    public void SetHeartsHealthSystem(HeartsHealthSystem heartsHealthSystem)
+    {
+        #region SetHeartsHealthSystem Function
+        this.heartsHealthSystem = heartsHealthSystem;
+
+        List<HeartsHealthSystem.Heart> heartList = heartsHealthSystem.GetHeartList();
+        Vector2 heartAnchoredPosition = new Vector2(0, 0);
+
+        for (int i = 0; i < heartList.Count; i++)
+        {
+            HeartsHealthSystem.Heart heart = heartList[i];
+            CreateHeartImage(heartAnchoredPosition).SetHeartFraments(heart.GetFragementsAmount());
+            heartAnchoredPosition += new Vector2(10, 0);
+        }
+        #endregion
+
+    }
+
+
+    private HeartImage CreateHeartImage(Vector2 anchoredPosition)
+    {
+        #region Creating Heart Visual
+        //Creating the game Object
+        GameObject heartGameObject = new GameObject("Heart",typeof(Image));
+
+        // Set as child of the transform
+        heartGameObject.transform.parent = transform;
+        heartGameObject.transform.localPosition = Vector3.zero;
+
+        //Find and Size heart
+        heartGameObject.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+        heartGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(30,30);
+
+        //Set heart Sprite
+        Image heartImageUI = heartGameObject.GetComponent<Image>();
+        heartImageUI.sprite = emptyHeartSprite;
+
+        HeartImage heartImage = new HeartImage (heartImageUI);
+        heartImageList.Add(heartImage);
+
+        return heartImage;
+        #endregion
+
+    }
+
+
+    // Resembles 1 Heart
+    public class HeartImage
+    {
+        #region HeartImageFunction
+        [Header("Variables")]
+
+        [SerializeField] Image heartImage;
+        [SerializeField] private HeartsHealthVisual heartsHealthVisual;
+        private Image heartImageUI;
+
+        public HeartImage(Image heartImageUI)
+        {
+            this.heartImageUI = heartImageUI;
+        }
+
+        public HeartImage(HeartsHealthVisual heartsHealthVisual,Image heartImage)
+        {
+            this.heartsHealthVisual = heartsHealthVisual;
+            this.heartImage = heartImage;
+            
+
+        }
+
+        public void  SetHeartFraments(int  fragments)
+        {
+            Debug.Log("heartImage = " + heartImage);
+            switch (fragments)
+            {
+                case 0: heartImage.sprite = heartsHealthVisual.fullHeartSprite; break;
+                case 1: heartImage.sprite = heartsHealthVisual.halfHeartSprite; break;
+                case 2: heartImage.sprite = heartsHealthVisual.emptyHeartSprite; break;
+            }
+        }
+        #endregion
+
+    }
+
+
+
+}
