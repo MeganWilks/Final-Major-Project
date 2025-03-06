73246afbdb2b17483df6bf7bedac217e5018da17
Megan Wilks <meganwilks18@gmail.com>
2025-03-06 13:47:45 +0000

    Fully Functional Coin Collection Method
    Coins Collected Function Complete

---
+++ Assets/Scripts/Inventory UI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventoryUI : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinText = GetComponent<TextMeshProUGUI>();
+        
+    }
+
+    public void UpdateCoinText(PlayerInventory playerInventory)
+    {
+        coinText.text = playerInventory.numOfCoin.ToString();
+
+    }
+
+    
+    
+}
