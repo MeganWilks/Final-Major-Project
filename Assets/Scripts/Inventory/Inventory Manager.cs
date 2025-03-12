7b0bf83861a0e6b4677daebefc0d6e6eea358731
Megan Wilks <meganwilks18@gmail.com>
2025-03-12 13:56:58 +0000

    Start Setup

--- Assets/Scripts/Inventory/Inventory Manager.cs
+++ Assets/Scripts/Inventory/Inventory Manager.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public ItemClass itemToAdd;
+    public ItemClass itemToRemove;
+
     public List<ItemClass> items = new List<ItemClass>();
 
+
+    public void Start()
+    {
+        Add(itemToAdd);
+        Remove(itemToRemove);
+    }
+    public void Add(ItemClass item)
+    {
+        items.Add(item);
+    }
+
+    public void Remove(ItemClass item)
+    {
+        items.Remove(item);
+
+    }
 }
