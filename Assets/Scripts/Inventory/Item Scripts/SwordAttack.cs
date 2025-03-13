7497db9ca6ae9007898aeb2b24dd29aed98e9aa8
Megan Wilks <meganwilks18@gmail.com>
2025-03-13 15:16:04 +0000

    Can Add Prefabs to Sword as New

--- Assets/Scripts/Inventory/Item Scripts/SwordAttack.cs
+++ Assets/Scripts/Inventory/Item Scripts/SwordAttack.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new Tool Class", menuName = "Item/Tool/Sword")]
 public class SwordAttack : ToolClass
 {
+
+    [Header("Sword Object")]
+
+    [SerializeField] public GameObject sword;
     public override void Use(PlayerController caller)
     {
         //base.Use(caller);
         Debug.Log("Use Sword");
+        Instantiate(sword, caller.transform.position, Quaternion.identity);
+        
     }
 
 
